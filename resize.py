2401e23db0b340031bc182a397d4f45f4bd4cf79
mani <e165731@ie.u-ryukyu.ac.jp>
2019-01-22 16:15:00 +0900

    resize

--- resize.py
+++ resize.py
@@ -1,21 +1,20 @@
-# resize.py
-# coding:utf-8
 import sys
 import os
-from PIL import Image
+import cv2
+args = sys.argv
 
 img_path = sys.argv[1]
-resize_width = float(sys.argv[2])
-img = Image.open(img_path)
+resize_height = float(sys.argv[2])
+img = cv2.imread(img_path)
 
 # 画像の解像度を取得して、リサイズする高さを計算
-img_width,img_height = img.size
-resize_height = resize_width / img_width * img_height
+img_height,img_width,_ = img.shape
+resize_width = resize_height / img_height * img_width
 
 # 画像をリサイズ
-img = img.resize((int(resize_width),int(resize_height)))
+img = cv2.resize(img,(int(resize_width),int(resize_height)))
 
 # リサイズした画像を(ファイル名-幅-高さ.拡張子)で保存
 root, ext = os.path.splitext(img_path)
 resize_img_path = "".join([root,"-%s-%s" % (int(resize_width),int(resize_height)),ext])
-img.save(resize_img_path)
+cv2.imwrite(resize_img_path,img)
