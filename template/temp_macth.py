45bd49bce9e97768638aa2c7635b1777417adcf9
Kohei Kina <e165747@ie.u-ryukyu.ac.jp>
2019-02-05 15:51:39 +0900

    temp_matchを変更

--- template/temp_macth.py
+++ template/temp_macth.py
@@ -4,29 +4,20 @@ from matplotlib import pyplot as plt
 import sys
 import math
 
-#画像をリサイズする関数
-def resize(img,resize_height):
-    img_height,img_width = img.shape
-    resize_width = resize_height / img_height * img_width
-    return cv2.resize(img,(int(resize_width),int(resize_height)))
-
 args = sys.argv
 img = cv2.imread(args[1],0)
-#画像をリサイズして縦の長さを合わせる
-#resize_height = 50
-#img = resize(img,50)
-
-#cv2.imshow("img",img)
-#cv2.waitKey()
 img2 = img.copy()
-#template = cv2.imread(args[2],0)
-#print(args[2])
-#w, h = template.shape[::-1]
 
+#マッチングさせる記号ファイルがある場所
 tempfile = ['../match_data/plus.png', '../match_data/minus.png', '../match_data/times.png',
             '../match_data/divide.png']
+#出力させる記号の配列
+sign = ["+","-","*","/"]
+max_sign = 0
+#最大類似度の場所
+place = 0
 #最大類似度
-t_o_m = []
+t_o_m = [0]
 for temp in tempfile:
     template = cv2.imread(temp,0)
     w, h = template.shape[::-1]
@@ -39,19 +30,19 @@ for temp in tempfile:
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
     print('max value: {},min_value: {}, position: {}'.format(max_val,min_val,max_loc))
 
-    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-    #if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-    #    top_left = min_loc
-    #else:
-    #    top_left = max_loc
     top_left = max_loc
     bottom_right = (top_left[0] + w, top_left[1] + h)
 
     cv2.rectangle(img,top_left, bottom_right, 0, 2)
-    t_o_m.append(max_val)
-    if max(t_o_m) >= 0.999:
-        print(max(t_o_m))
 
+    #類似度を元に文字を判定
+    if max_val >= max(t_o_m) and max_val >= 0.8:
+        place = tempfile.index(temp)
+        print(tempfile.index(temp))
+        max_sign = sign[place]
+
+    t_o_m.append(max_val)
+    print(max_sign)
     #出力
     plt.subplot(221),plt.imshow(res,cmap = 'gray')
     plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
