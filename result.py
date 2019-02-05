761aec01607e12aaab74717f4716eba22167e474
Kohei Kina <e165747@ie.u-ryukyu.ac.jp>
2019-02-05 15:57:39 +0900

    temp_matchを変更

--- result.py
+++ result.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
+import sys
 
+args = sys.argv
 #######   training part    ###############
 samples = np.loadtxt('generalsamples.data',np.float32)
 responses = np.loadtxt('generalresponses.data',np.float32)
@@ -11,18 +13,21 @@ model.train(samples,cv2.ml.ROW_SAMPLE,responses)
 
 ############################# testing part  #########################
 
-im = cv2.imread('SS2.png')
+im = cv2.imread(args[1])
 out = np.zeros(im.shape,np.uint8)
 gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
 thresh = cv2.adaptiveThreshold(gray,255,1,1,11,2)
 
 image,contours,hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 
+last_x = 0
+#print(contours)
 for cnt in contours:
     if cv2.contourArea(cnt)>40:
         [x,y,w,h] = cv2.boundingRect(cnt)
-        next_x = x
-        if  h>40:
+       # print(x)
+        if  h>53:
+           # if x -last_x >= 5:  
             cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
             roi = thresh[y:y+h,x:x+w]
             roismall = cv2.resize(roi,(10,10))
@@ -31,6 +36,10 @@ for cnt in contours:
             retval, results, neigh_resp, dists = model.findNearest(roismall, k = 1)
             string = str(int((results[0][0])))
             cv2.putText(out,string,(x,y+h),0,1,(0,255,0))
+            last_x = x
+           # else:
+           #    lastx_ = x 
+
             """
             ファイルに書き込み
             with open('test.txt', 'a', encoding='utf-8') as f:
